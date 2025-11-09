a460de44d173ac697d10871737c0965622ec0f8d
Diana Kozachenko <kozachenkodiana03@mail.ru>
2025-11-09 15:44:36 +0100

    MVP

--- main.js
+++ main.js
@@ -7,10 +7,27 @@ const WEDNESDAY_KEY = 'wednesday';
 const THURSDAY_KEY = 'thursday';
 const FRIDAY_KEY = 'friday';
 const SATURDAY_KEY = 'saturday';
+const SUNDAY_KEY = 'sunday';
+
+const DAY_LABEL_MAP = {
+  [MONDAY_KEY]: 'Понедельник',
+  [TUESDAY_KEY]: 'Вторник',
+  [WEDNESDAY_KEY]: 'Среда',
+  [THURSDAY_KEY]: 'Четверг',
+  [FRIDAY_KEY]: 'Пятница',
+  [SATURDAY_KEY]: 'Суббота',
+  [SUNDAY_KEY]: 'Воскресенье'
+};
 
 const BREAKFAST_KEY = 'breakfast';
 const LUNCH_KEY = 'lunch';
 const DINNER_KEY = 'dinner';
+
+const MEAL_LABEL_MAP = {
+  [BREAKFAST_KEY]: 'Завтрак',
+  [LUNCH_KEY]: 'Обед',
+  [DINNER_KEY]: 'Ужин'
+}
 
 const textTextarea = document.querySelector('#text');
 textTextarea.addEventListener('paste', onPasteText);
@@ -35,12 +52,12 @@ function onPasteText(event) {
 
 function parseMenu(match) {
   const menu = {
-    monday: {},
-    tuesday: {},
-    wednesday: {},
-    thursday: {},
-    friday: {},
-    saturday: {}
+    [MONDAY_KEY]: {},
+    [TUESDAY_KEY]: {},
+    [WEDNESDAY_KEY]: {},
+    [THURSDAY_KEY]: {},
+    [FRIDAY_KEY]: {},
+    [SATURDAY_KEY]: {}
   };
 
   const INDEX_MENU_KEY_MAP = {
@@ -120,10 +137,151 @@ function setSecondStep(menu) {
   `;
 
   const button = document.querySelector('.button');
-  button.addEventListener('click', setThirdStep)
+  button.addEventListener('click', () => {
+    const includeSundayCheckbox = document.querySelector('#include-sunday');
+    setThirdStep(menu, includeSundayCheckbox.checked);
+  })
 }
 
-function setThirdStep() {
-  const includeSundayCheckbox = document.querySelector('#include-sunday');
-  console.warn(includeSundayCheckbox.checked);
+function setThirdStep(menu, includeSunday) {
+  const flowStepBlock = document.querySelector('.flow-step');
+  flowStepBlock.innerHTML = `
+    <form class="form">
+      ${buildDayFieldset(MONDAY_KEY, menu, MONDAY_KEY, TUESDAY_KEY)}
+
+      ${buildDayFieldset(TUESDAY_KEY, menu, MONDAY_KEY, TUESDAY_KEY)}
+
+      ${buildDayFieldset(WEDNESDAY_KEY, menu, WEDNESDAY_KEY, THURSDAY_KEY)}
+
+      ${buildDayFieldset(THURSDAY_KEY, menu, WEDNESDAY_KEY, THURSDAY_KEY)}
+
+      ${buildDayFieldset(FRIDAY_KEY, menu, FRIDAY_KEY, SATURDAY_KEY)}
+
+      ${buildDayFieldset(SATURDAY_KEY, menu, FRIDAY_KEY, SATURDAY_KEY)}
+
+      ${includeSunday ? buildDayFieldset(SUNDAY_KEY, menu, FRIDAY_KEY, SATURDAY_KEY) : ''}
+
+      <button class="button" type="submit">Сформировать</button>
+    </form>
+  `;
+
+  const formButton = document.querySelector('.button');
+  formButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    setFourthStep(menu, includeSunday);
+  });
+}
+
+function buildMealFieldset(dayKey, mealKey, menu, day1ForChoose, day2ForChoose) {
+  return `
+    <fieldset class="meal-fieldset">
+      <legend>${MEAL_LABEL_MAP[mealKey]}</legend>
+
+      <div>
+        <input type="radio" name="${dayKey}-${mealKey}" id="${dayKey}-${day1ForChoose}-${mealKey}-1" value="${menu[day1ForChoose][mealKey].firstOption}" />
+        <label for="${dayKey}-${day1ForChoose}-${mealKey}-1">${menu[day1ForChoose][mealKey].firstOption}</label>
+      </div>
+
+      <div>
+        <input type="radio" name="${dayKey}-${mealKey}" id="${dayKey}-${day1ForChoose}-${mealKey}-2" value="${menu[day1ForChoose][mealKey].secondOption}" />
+        <label for="${dayKey}-${day1ForChoose}-${mealKey}-2">${menu[day1ForChoose][mealKey].secondOption}</label>
+      </div>
+
+      <div>
+        <input type="radio" name="${dayKey}-${mealKey}" id="${dayKey}-${day2ForChoose}-${mealKey}-1" value="${menu[day2ForChoose][mealKey].firstOption}" />
+        <label for="${dayKey}-${day2ForChoose}-${mealKey}-1">${menu[day2ForChoose][mealKey].firstOption}</label>
+      </div>
+
+      <div>
+        <input type="radio" name="${dayKey}-${mealKey}" id="${dayKey}-${day2ForChoose}-${mealKey}-2" value="${menu[day2ForChoose][mealKey].secondOption}" />
+        <label for="${dayKey}-${day2ForChoose}-${mealKey}-2">${menu[day2ForChoose][mealKey].secondOption}</label>
+      </div>
+    </fieldset>
+  `;
+}
+
+function buildDayFieldset(dayKey, menu, day1ForChoose, day2ForChoose) {
+  return `
+    <fieldset class="day-fieldset">
+      <legend>${DAY_LABEL_MAP[dayKey]}</legend>
+
+      ${this.buildMealFieldset(dayKey, BREAKFAST_KEY, menu, day1ForChoose, day2ForChoose)}
+
+      ${this.buildMealFieldset(dayKey, LUNCH_KEY, menu, day1ForChoose, day2ForChoose)}
+
+      ${this.buildMealFieldset(dayKey, DINNER_KEY, menu, day1ForChoose, day2ForChoose)}
+    </fieldset>
+  `;
+}
+
+function setFourthStep(menu, includeSunday) {
+  const form = document.querySelector('form');
+  const formData = new FormData(form);
+  const formValue = Object.fromEntries(formData.entries());
+
+  let validKeysLength = includeSunday ? 3 * 7 : 3 * 6;
+  if (Object.keys(formValue).length !== validKeysLength) {
+    return;
+  }
+
+  const mondayTuesdayMeals = [
+    formValue[`${MONDAY_KEY}-${BREAKFAST_KEY}`], formValue[`${MONDAY_KEY}-${LUNCH_KEY}`], formValue[`${MONDAY_KEY}-${DINNER_KEY}`],
+    formValue[`${TUESDAY_KEY}-${BREAKFAST_KEY}`], formValue[`${TUESDAY_KEY}-${LUNCH_KEY}`], formValue[`${TUESDAY_KEY}-${DINNER_KEY}`],
+  ];
+
+  const wednesdayThursdayMeals = [
+    formValue[`${WEDNESDAY_KEY}-${BREAKFAST_KEY}`], formValue[`${WEDNESDAY_KEY}-${LUNCH_KEY}`], formValue[`${WEDNESDAY_KEY}-${DINNER_KEY}`],
+    formValue[`${THURSDAY_KEY}-${BREAKFAST_KEY}`], formValue[`${THURSDAY_KEY}-${LUNCH_KEY}`], formValue[`${THURSDAY_KEY}-${DINNER_KEY}`],
+  ];
+
+  const lastDayMeals = [
+    formValue[`${FRIDAY_KEY}-${BREAKFAST_KEY}`], formValue[`${FRIDAY_KEY}-${LUNCH_KEY}`], formValue[`${FRIDAY_KEY}-${DINNER_KEY}`],
+    formValue[`${SATURDAY_KEY}-${BREAKFAST_KEY}`], formValue[`${SATURDAY_KEY}-${LUNCH_KEY}`], formValue[`${SATURDAY_KEY}-${DINNER_KEY}`],
+  ];
+
+  if (includeSunday) {
+    lastDayMeals.push(formValue[`${SUNDAY_KEY}-${BREAKFAST_KEY}`], formValue[`${SUNDAY_KEY}-${LUNCH_KEY}`], formValue[`${SUNDAY_KEY}-${DINNER_KEY}`]);
+  }
+
+  const mondayTuesdayMealsDistribution = getMealsDistributionWithCount(mondayTuesdayMeals);
+  const wednesdayThursdayMealsDistribution = getMealsDistributionWithCount(wednesdayThursdayMeals);
+  const lastDayMealsDistribution = getMealsDistributionWithCount(lastDayMeals);
+
+  const flowStepBlock = document.querySelector('.flow-step');
+  flowStepBlock.innerHTML = `
+    <p class="result-text">
+      <strong>Пн+Вт:</strong><br/><br/>
+
+      ${buildMealsWithCount(mondayTuesdayMealsDistribution)}<br/>
+
+      <strong>Ср+Чт:</strong><br/><br/>
+
+      ${buildMealsWithCount(wednesdayThursdayMealsDistribution)}<br/>
+
+      <strong>Пт+Сб${includeSunday ? '+Вс' : ''}:</strong><br/><br/>
+
+      ${buildMealsWithCount(lastDayMealsDistribution)}
+    </p>
+  `;
+
+}
+
+function getMealsDistributionWithCount(meals) {
+  const result = {};
+
+  meals.forEach(meal => {
+    if (result[meal] === undefined) {
+      result[meal] = 0;
+    }
+
+    ++result[meal];
+  });
+
+  return result;
+}
+
+function buildMealsWithCount(mealsDistribution) {
+  return `
+    ${Object.entries(mealsDistribution).map(([meal, count]) => `${meal} x${count}<br/>`).join('')}
+  `;
 }
